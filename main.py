58ff14cdfbfe447f56aad3def0dde25f0c15203f
woong <jykv588@naver.com>
2024-04-29 15:35:11 +0900

    두번째커밋 마이너스 추가

--- main.py
+++ main.py
@@ -3,8 +3,16 @@ def add_func(n1,n2):
    retVal = n1+n2
    return retVal
 
+def add_func(n1,n2):
+   retVal = n1-n2
+   return retVal
+
+
 ## 전역 변수(=클래스 변수)
 num1,num2,res=100,200,0
 ## 메인 함수 부분
 res=add_func(num1,num2)
-print(num1,'+',num2,'=',res)  
+print(num1,'+',num2,'=',res)
+
+res=add_func(num1,num2)
+print(num1,'-',num2,'=',res)  
