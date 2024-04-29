d53d767d655062dd6b2fd071f0cc7a2516278753
woong <jykv588@naver.com>
2024-04-29 15:41:25 +0900

    세번째 네번째 커밋 한번에 했음 곱하기 나누기 추가했다

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ def sub_func(n1,n2):
    retVal = n1-n2
    return retVal
 
+def mul_func(n1,n2):
+   retVal = n1*n2
+   return retVal
+
+def div_func(n1,n2):
+   retVal = n1/n2
+   return retVal
+
 
 ## 전역 변수(=클래스 변수)
 num1,num2,res=100,200,0
@@ -14,5 +22,11 @@ num1,num2,res=100,200,0
 res=add_func(num1,num2)
 print(num1,'+',num2,'=',res)
 
-res=add_func(num1,num2)
-print(num1,'-',num2,'=',res)  
+res=sub_func(num1,num2)
+print(num1,'-',num2,'=',res)
+
+res=mul_func(num1,num2)
+print(num1,'*',num2,'=',res)
+
+res=div_func(num1,num2 )
+print(num1,'/',num2,'=',res)  
